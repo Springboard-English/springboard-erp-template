97555daa3915cc2ddce10e4fa99ab6557a3ac869
Nguyen Vu Hai An <56158456+andrew-hnn@users.noreply.github.com>
2026-05-15 17:14:17 +0700

    Switch exports to relative paths
    Export DetailActionPanel, DetailTabs, and DetailTabbedSection from
    DetailLayout

--- src/exports.ts
+++ src/exports.ts
@@ -1,19 +1,19 @@
-export { default as App } from "@/App";
-export type { AppProps, AuthViewMode } from "@/App";
+export { default as App } from "./App";
+export type { AppProps, AuthViewMode } from "./App";
 
-export { default as SignIn } from "@/views/SignIn";
-export type { SignInViewProps } from "@/views/SignIn";
-export { default as ResetPassword } from "@/views/ResetPassword";
-export type { ResetPasswordViewProps } from "@/views/ResetPassword";
+export { default as SignIn } from "./views/SignIn";
+export type { SignInViewProps } from "./views/SignIn";
+export { default as ResetPassword } from "./views/ResetPassword";
+export type { ResetPasswordViewProps } from "./views/ResetPassword";
 
-export { AuthProvider, useAuth } from "@/context/AuthContext";
-export { default as AppTheme } from "@/theme/AppTheme";
-export { default as ColorModeSelect } from "@/theme/ColorModeSelect";
+export { AuthProvider, useAuth } from "./context/AuthContext";
+export { default as AppTheme } from "./theme/AppTheme";
+export { default as ColorModeSelect } from "./theme/ColorModeSelect";
 
-export { default as ForgotPassword } from "@/components/ForgotPassword";
-export { default as StatusBanner } from "@/components/StatusBanner";
-export { default as SimpleDataTable } from "@/components/SimpleDataTable";
-export { default as FormTableDialog, FormTableRow } from "@/components/dialogs/FormTableDialog";
+export { default as ForgotPassword } from "./components/ForgotPassword";
+export { default as StatusBanner } from "./components/StatusBanner";
+export { default as SimpleDataTable } from "./components/SimpleDataTable";
+export { default as FormTableDialog, FormTableRow } from "./components/dialogs/FormTableDialog";
 export {
   DetailView,
   DetailHeader,
@@ -25,18 +25,21 @@ export {
   DetailFieldsTable,
   DetailClassLinkValue,
   DetailClassHeaderLabel,
+  DetailActionPanel,
+  DetailTabs,
+  DetailTabbedSection,
   BackgroundDetailViewContext,
-} from "@/components/layout/DetailLayout";
-export { default as ProgressMetricCell } from "@/components/cells/ProgressMetricCell";
+} from "./components/layout/DetailLayout";
+export { default as ProgressMetricCell } from "./components/cells/ProgressMetricCell";
 
-export * from "@/components/ui/button";
-export * from "@/components/ui/card";
-export * from "@/components/ui/dialog";
-export * from "@/components/ui/dropdown-menu";
-export * from "@/components/ui/input";
-export * from "@/components/ui/label";
-export * from "@/components/ui/searchable-select";
-export * from "@/components/ui/separator";
-export * from "@/components/ui/sheet";
-export * from "@/components/ui/table";
-export * from "@/components/ui/textarea";
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/dialog";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/input";
+export * from "./components/ui/label";
+export * from "./components/ui/searchable-select";
+export * from "./components/ui/separator";
+export * from "./components/ui/sheet";
+export * from "./components/ui/table";
+export * from "./components/ui/textarea";
